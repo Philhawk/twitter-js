9371ff86b3fc99904f82f8a3c944a88a2d0c7efb
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-09-19 18:01:20 -0400

    setup middleware

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var morgan = require('morgan');
 var app = express();
+
+app.use(morgan('dev'))
 
 app.get('/', function(req, res){
   res.send('Hello World');
   console.log('Listening and no problems');
 });
+
+app.get('/news', function(req, res){
+  res.send('News Page');
+  console.log('Visiting the news page');
+})
 
 app.listen(3000);
