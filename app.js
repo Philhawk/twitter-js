5116bb76eb47badb82a34800bc00f5d43466bf42
George Smith-Sweeper <george.smsweeper@gmail.com>
2016-09-21 16:40:58 -0400

    added a middleware comment

--- app.js
+++ app.js
@@ -9,10 +9,9 @@ var io = socketio.listen(server);
 var bodyParser = require('body-parser')
 
 app.set('view engine', 'html');
-app.use(morgan('dev'))
+app.use(morgan('dev')) // logging middleware
 app.use(bodyParser.urlencoded({extended: true}));
 app.engine('html', nunjucks.render);
-
 app.use('/', routes(io));
 app.use('/static', express.static('public'));
 
